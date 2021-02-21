a5aecce409f447835cacff41d480b60b60128c4c
subinps57 <76514033+subinps57@users.noreply.github.com>
2021-02-21 12:49:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,7 +1,7 @@
 import os
 import logging
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from info import START_MSG, CHANNELS, ADMINS, INVITE_MSG
 from utils import Media
 
